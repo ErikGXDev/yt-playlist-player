4ef5d761ec6de3e78233d0866ca995124c2acee7
ErikoX <54033728+ErikoXDev@users.noreply.github.com>
2023-04-14 13:52:49 +0200

    utility functions and types

--- src/components/util/time.ts
+++ src/components/util/time.ts
@@ -1,17 +1,51 @@
-const toHHMMSS = function (str: Number) {
-  var sec_num = parseInt(str.toString(), 10); // don't forget the second param
-  var hours = Math.floor(sec_num / 3600);
-  var minutes = Math.floor((sec_num - hours * 3600) / 60);
-  var seconds = sec_num - hours * 3600 - minutes * 60;
+function iso8601DurationToSeconds(duration: string): number {
+  const matches = duration.match(/PT(\d+H)?(\d+M)?(\d+S)?/);
 
-  var _minutes = "" + minutes;
-  var _seconds = "" + seconds;
-  if (minutes < 10) {
-    _minutes = "0" + minutes;
+  const hours = matches?.[1] ? parseInt(matches[1].slice(0, -1)) : 0;
+  const minutes = matches?.[2] ? parseInt(matches[2].slice(0, -1)) : 0;
+  const seconds = matches?.[3] ? parseInt(matches[3].slice(0, -1)) : 0;
+
+  return hours * 3600 + minutes * 60 + seconds;
+}
+
+function toHHMMSS(seconds: number): string {
+  const hours = Math.floor(seconds / 3600);
+  const minutes = Math.floor((seconds % 3600) / 60);
+  const remainingSeconds = seconds % 60;
+
+  let timeString = "";
+
+  if (hours > 0) {
+    timeString += hours.toString().padStart(2, "0") + ":";
   }
-  if (seconds < 10) {
-    _seconds = "0" + seconds;
+
+  timeString +=
+    minutes.toString().padStart(2, "0") +
+    ":" +
+    remainingSeconds.toString().padStart(2, "0");
+
+  return timeString;
+}
+
+function to_h_min(seconds: number): string {
+  const hours = Math.floor(seconds / 3600);
+  const minutes = Math.floor((seconds % 3600) / 60);
+
+  let timeString = "";
+
+  if (hours > 0) {
+    timeString += hours.toString() + "h ";
   }
-  return _minutes + ":" + _seconds;
-};
-export { toHHMMSS };
+
+  if (minutes > 0) {
+    timeString += minutes.toString() + "min";
+  }
+
+  if (timeString === "") {
+    timeString = "0min";
+  }
+
+  return timeString;
+}
+
+export { toHHMMSS, to_h_min, iso8601DurationToSeconds };
---
+++ src/components/util/youtubeTypes.ts
@@ -0,0 +1,97 @@
+export type PlaylistItemType = {
+  kind: string;
+  etag: string;
+  id: string;
+  snippet: {
+    publishedAt: string;
+    channelId: string;
+    title: string;
+    description: string;
+    duration: string;
+    stream: string;
+    thumbnails: {
+      default: { url: string; width: number; height: number };
+      medium: { url: string; width: number; height: number };
+      high: { url: string; width: number; height: number };
+      standard: { url: string; width: number; height: number };
+      maxres: { url: string; width: number; height: number };
+    };
+    channelTitle: string;
+    playlistId: string;
+    position: number;
+    resourceId: {
+      kind: string;
+      videoId: string;
+    };
+    videoOwnerChannelTitle: string;
+    videoOwnerChannelId: string;
+  };
+  contentDetails: {
+    videoId: string;
+    videoPublishedAt: string;
+  };
+};
+
+export type PlaylistType = {
+  ownerid: string;
+  name: string;
+  owner: string;
+  visibility: string;
+  items: PlaylistItemType[];
+};
+
+export type Channel = {
+  id: string;
+  snippet: {
+    title: string;
+    thumbnails: {
+      default: { url: string };
+      medium: { url: string };
+      high: { url: string };
+    };
+    description: string;
+  };
+};
+
+export type FullChannel = {
+  kind: string;
+  etag: string;
+  id: string;
+  snippet: {
+    title: string;
+    description: string;
+    customUrl: string;
+    publishedAt: string;
+    thumbnails: {
+      default: {
+        url: string;
+        width: number;
+        height: number;
+      };
+      medium: {
+        url: string;
+        width: number;
+        height: number;
+      };
+      high: {
+        url: string;
+        width: number;
+        height: number;
+      };
+    };
+    localized: {
+      title: string;
+      description: string;
+    };
+  };
+  contentDetails: {
+    relatedPlaylists: {
+      likes: string;
+      uploads: string;
+    };
+  };
+};
+
+export type YoutubeData = {
+  channel: FullChannel;
+};
